5c49361c0e3ea74a52216c06c240c08cd634dcb3
vinicios-goncalves <viniciusgonca9@gmail.com>
2025-05-04 19:36:47 -0300

    2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("primeiro hello word 1");
+        System.out.println("vinicios");
     }
 }
